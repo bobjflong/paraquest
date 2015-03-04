fc06e3572dfeda999b4aa35ca12f390484156c18
Bob Long <robertjflong@gmail.com>
2015-03-04 21:08:25 +0000

    Refactoring Client validations

--- lib/paraquest/http_client.rb
+++ lib/paraquest/http_client.rb
@@ -2,12 +2,17 @@ require 'http_client'
 
 module Paraquest
   def self.Client(*args)
-    if args.length == 1 && args.fetch(0).is_a?(String)
+    if first_argument_is_a?(args, String)
       ::HTTP::Client.new(default_host: args.fetch(0))
-    elsif args.length == 1 && args.fetch(0).is_a?(Hash)
+    elsif first_argument_is_a?(args, Hash)
       ::HTTP::Client.new(args.fetch(0))
     else
       raise ArgumentError.new('Arguments to Client must be a String or Hash')
     end
   end
+
+  def self.first_argument_is_a?(o, klass)
+    o.length == 1 && o.first.is_a?(klass)
+  end
+  private_class_method :first_argument_is_a?
 end
